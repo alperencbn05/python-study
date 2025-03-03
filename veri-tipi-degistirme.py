3c5cdf2d0d945078436795093e9562bec78a5f36
Alperen Çoban <alperencoban474@gmail.com>
2025-03-03 06:13:35 +0300

    bolum-1

---
+++ veri-tipi-degistirme.py
@@ -0,0 +1,11 @@
+sayi1 = int(input("sayi1 : "))
+sayi2 = int(input("sayi2 : "))
+
+
+toplam = sayi1+sayi2
+print(toplam)
+
+# x = int("10")  10 olarak gider
+# y = int("10.5") hata meydana gelir
+# z = float("10.5") 10.5 olarak yazar
+# q = float(10) 10.0 olarak yazar
